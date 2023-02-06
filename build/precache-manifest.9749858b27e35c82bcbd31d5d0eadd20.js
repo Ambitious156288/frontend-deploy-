11da1e55a3de507341f1523bd2447defd4755564
kk <krystian.kalwik@gmail.com>
2023-02-06 17:39:51 +0100

    aaa

---
+++ build/precache-manifest.9749858b27e35c82bcbd31d5d0eadd20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e72a78f723201545c8c37c4aa71f2a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "101451da317134f4b64a",
+    "url": "/static/css/2.ed710ba4.chunk.css"
+  },
+  {
+    "revision": "9339fecbcede21648876",
+    "url": "/static/css/main.70a1af94.chunk.css"
+  },
+  {
+    "revision": "101451da317134f4b64a",
+    "url": "/static/js/2.cbeca6d3.chunk.js"
+  },
+  {
+    "revision": "7542c89073b3438ea7b25993dc26c603",
+    "url": "/static/js/2.cbeca6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9339fecbcede21648876",
+    "url": "/static/js/main.acd32c1b.chunk.js"
+  },
+  {
+    "revision": "42269de3bf798174a42a",
+    "url": "/static/js/runtime-main.d5f06370.js"
+  }
+]);
